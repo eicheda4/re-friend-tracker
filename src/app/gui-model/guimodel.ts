662b08e7cccad99c9f7caa615b6367657f7c3dfc
eicheda4 <eicheda4@students.zhaw.ch>
2021-12-09 18:23:32 +0000

    Name geändert und Nickname hinzugefügt

--- src/app/gui-model/guimodel.ts
+++ src/app/gui-model/guimodel.ts
@@ -6,7 +6,7 @@ export class GuiModel {
 
     private _guiModel = {
         "application": {
-            "title": "Requirements Engineering Friend Tracker",
+            "title": "Davides Friend Tracker",
             "formList": [
                 {
                     "id": "FriendForm",
@@ -26,6 +26,13 @@ export class GuiModel {
                             "name": "FirstName",
                             "width": 1,
                             "required": true
+                        },
+                        { 
+                            "id": "nickname", 
+                            "type": "text", 
+                            "name": "Nickname", 
+                            "width": 2, 
+                            "required": true 
                         },
                         {
                             "id":   "location",
